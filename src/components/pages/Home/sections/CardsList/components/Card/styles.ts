ba526543305d9cf1cd6bb357b1637bae36cf4fe3
MatheusFerrari <Matheusferrariaguirre@gmail.com>
2025-01-29 20:56:25 -0300

    refactor: chenging px to rem

--- src/components/pages/Home/sections/CardsList/components/Card/styles.ts
+++ src/components/pages/Home/sections/CardsList/components/Card/styles.ts
@@ -1,22 +1,22 @@
 // External libraries
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 interface IPropsButton {
   $image: string
 }
 
 export const StyledButton = styled.button<IPropsButton>`
-  background-image: url(${({$image}) => $image});
+  background-image: url(${({ $image }) => $image});
   background-size: cover;
   background-position: center;
-  width: 120px;
-  height: 120px;
+  width: 7.5rem;
+  height: 7.5rem;
   border: none;
   cursor: pointer;
-  border-radius: 10px;
+  border-radius: 0.65rem;
   transition: transform 0.2s;
 
   &:hover {
     transform: scale(1.1);
   }
-`;
+`
