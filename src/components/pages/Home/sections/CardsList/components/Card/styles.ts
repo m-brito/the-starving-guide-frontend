fe93c4d5f430764bf1463914c619e443d62d917b
FCNohama <nohamaoficial@gmail.com>
2025-01-29 20:45:48 -0300

    refactor: reorganize `styledbutton` props

--- src/components/pages/Home/sections/CardsList/components/Card/styles.ts
+++ src/components/pages/Home/sections/CardsList/components/Card/styles.ts
@@ -1,8 +1,12 @@
 // External libraries
 import styled from 'styled-components';
 
-export const StyledButton = styled.button<{ $image: string }>`
-  background-image: url(${(props) => props.$image});
+interface IPropsButton {
+  $image: string
+}
+
+export const StyledButton = styled.button<IPropsButton>`
+  background-image: url(${({$image}) => $image});
   background-size: cover;
   background-position: center;
   width: 120px;
