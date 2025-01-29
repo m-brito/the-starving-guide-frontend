a640e2b1048c3d72933c4041568f47902d458d25
FCNohama <nohamaoficial@gmail.com>
2025-01-29 20:27:53 -0300

    refactor: reorganize `rendercards` function

--- src/components/pages/Home/sections/CardsList/index.tsx
+++ src/components/pages/Home/sections/CardsList/index.tsx
@@ -9,19 +9,18 @@ import { CardsGrid } from './styles';
 
 export const CardsList: React.FC = () => {
   const cards = [
-    { cardImage: '/cards/character_card.png', altText: 'Characters Card'},
-    { cardImage: '/cards/mobs_card.png', altText: 'Mobs Card'},
-    { cardImage: '/cards/foods_card.png', altText: 'Foods Card'},
-    { cardImage: '/cards/biomes_card.png', altText: 'Biomes Card'},
-    { cardImage: '/cards/seasons_card.png', altText: 'Seasons Card'},
-    { cardImage: '/cards/items_card.png', altText: 'Items Card'},
+    { cardImage: '/cards/character_card.png', altText: 'Characters Card', onClick: () => console.log('Clicked on Character Card') },
+    { cardImage: '/cards/mobs_card.png', altText: 'Mobs Card', onClick: () => console.log('Clicked on Mob Card') },
+    { cardImage: '/cards/foods_card.png', altText: 'Foods Card', onClick: () => console.log('Clicked on Food Card') },
+    { cardImage: '/cards/biomes_card.png', altText: 'Biomes Card', onClick: () => console.log('Clicked on Biome Card') },
+    { cardImage: '/cards/seasons_card.png', altText: 'Seasons Card', onClick: () => console.log('Clicked on Season Card') },
+    { cardImage: '/cards/items_card.png', altText: 'Items Card', onClick: () => console.log('Clicked on Item Card') },
   ];
 
-  return (
-    <CardsGrid>
-      {cards.map((card, index) => (
-        <Card key={index} cardImage={card.cardImage} altText={card.altText} />
-      ))}
-    </CardsGrid>
-  );
+  const renderCards = () =>
+    cards.map((card, index) => (
+      <Card key={index} cardImage={card.cardImage} altText={card.altText} onClick={card.onClick} />
+    ));
+    
+  return <CardsGrid>{renderCards()}</CardsGrid>;
 };
