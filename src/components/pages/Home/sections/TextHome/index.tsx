fe25736cb74510d5773e8cd2d5d125312978ff55
MatheusFerrari <Matheusferrariaguirre@gmail.com>
2025-02-09 23:26:48 -0300

    feat: create TextHome component

---
+++ src/components/pages/Home/sections/TextHome/index.tsx
@@ -0,0 +1,35 @@
+// External Libraries
+import React from 'react'
+
+// Styles
+import {
+  Container,
+  ImageContainer,
+  Image,
+  TextContainer,
+  Title,
+  Description
+} from './styles'
+
+export const TextHome = () => {
+  return (
+    <Container>
+      <ImageContainer>
+        <Image
+          src={'./general/TextHomeImage.png'}
+          alt="Boss de Don't Starve Together"
+        />
+      </ImageContainer>
+      <TextContainer>
+        <Title>GAME</Title>
+        <Description>
+          Don`t Starve Together é um jogo de sobrevivência cooperativo
+          desafiador, ambientado em um mundo sombrio e misterioso repleto de
+          criaturas estranhas, perigos inesperados e segredos ocultos. Criado
+          pela Klei Entertainment, o jogo combina estratégia, exploração e um
+          toque de humor macabro.
+        </Description>
+      </TextContainer>
+    </Container>
+  )
+}
